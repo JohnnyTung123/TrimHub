4ddbcb7fba1903eaad74ce73603b170cba1ef34d
JohnnyTung123 <johnnya12399@gmail.com>
2024-03-07 17:40:43 +0800

    HomePage set up

--- src/index.js
+++ src/index.js
@@ -1,7 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* <Route path="/" element={<LoginPage />} /> */}
+        <Route index element={<HomePage />} />
+        {/* <Route path="register/:userType" element={<RegisterPage />} /> */}
+        {/* <Route element={<ProtectedRoutes />}>
+          <Route path="patient" element={<PatientPage />} />
+          <Route path="doctor" element={<DoctorPage />} />
+        </Route> */}
+        {/* <Route path="*" element={<NoPage />} /> */}
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
