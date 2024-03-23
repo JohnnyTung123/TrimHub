be255ab59ee749d3ba9371390f7fe4b7463a138d
JohnnyTung123 <johnnya12399@gmail.com>
2024-03-23 20:51:13 +0800

    Johnny merge main

--- frontend/src/pages/ProfilePage/index.jsx
+++ frontend/src/pages/ProfilePage/index.jsx
@@ -1,36 +1,60 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useMemo } from "react";
+import Cookies from "universal-cookie";
 import "./ProfilePage.css";
+import { useNavigate } from "react-router-dom";
 
 const ProfilePage = () => {
-    const username = 'username';
-    const email = 'abc@example.com';
+  const cookies = useMemo(() => new Cookies(), []);
+  const navigate = useNavigate();
+  const [user, setUser] = useState({});
+  const [dropdownOpen, setDropdownOpen] = useState(false);
 
-    const [dropdownOpen, setDropdownOpen] = useState(false);
-  
-    // Placeholder functions for handling button clicks
-    const handleChangePassword = () => {
-      // Logic to change password
-    };
-  
-    const handleLogout = () => {
-      // Logic to log out
-    };
-  
-    return (
-      <div className="profile-page">
-        <header className="profile-header">
+  // authenticate user
+  useEffect(() => {
+    console.log("useEffect triggered");
+    fetch("http://localhost:8080/auth/endpoint", {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${cookies.get("auth")}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setUser(data.user);
+      })
+      .catch((err) => console.error(err));
+  }, [cookies]);
+
+  const handleLogout = () => {
+    cookies.remove("auth");
+    setUser(null);
+  };
+
+  // Placeholder functions for handling button clicks
+  const handleChangePassword = () => {
+    // Logic to change password
+  };
+
+  return (
+    <div className="profile-page">
+      <header className="profile-header">
         <h1>Trim Hub</h1>
         <nav>
           <a href="/men">Men</a> | <a href="/women">Woman</a>
           <div className="dropdown">
-            <button className="dropbtn" onClick={() => setDropdownOpen(!dropdownOpen)}>{username}
-            <span className="icons"> ▽</span>
+            <button
+              className="dropbtn"
+              onClick={() => setDropdownOpen(!dropdownOpen)}
+            >
+              {user.username}
+              <span className="icons"> ▽</span>
             </button>
             {dropdownOpen && (
               <div className="dropdown-content">
                 <div className="user-info">
                   <div className="avatar">{/* User Avatar Image */}</div>
-                  <div className="username">{username}</div>
+                  <div className="username">{user.username}</div>
                 </div>
                 <a href="/profile">User information</a>
                 <a href="/bookings">Bookings</a>
@@ -42,18 +66,30 @@ const ProfilePage = () => {
           </div>
         </nav>
       </header>
-        <div className="profile-content">
-          <main className="profile-main-content">
-            <div className="profile-email-section">
-              <label htmlFor="email">Email</label>
-              <input id="profile-email" type="email" value={email} readOnly />
-            </div>
-            <button onClick={handleChangePassword}>Change Password</button>
-            <button onClick={handleLogout}>Log out</button>
-          </main>
-        </div>
+      <div className="profile-content">
+        <main className="profile-main-content">
+          <div className="profile-email-section">
+            <label htmlFor="email">Email</label>
+            <input
+              id="profile-email"
+              type="email"
+              value={user.email}
+              readOnly
+            />
+            <label htmlFor="username">Username</label>
+            <input
+              id="profile-username"
+              type="text"
+              value={user.username}
+              readOnly
+            />
+          </div>
+          <button onClick={handleChangePassword}>Change Password</button>
+          <button onClick={handleLogout}>Log out</button>
+        </main>
       </div>
-    );
-  }
+    </div>
+  );
+};
 
 export default ProfilePage;
