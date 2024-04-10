d4b7b8076048a656c4f9c03fe5b1dd7c6c1d4b81
JohnnyTung123 <johnnya12399@gmail.com>
2024-04-10 17:44:32 +0800

    like/dislike salon completed

--- frontend/src/pages/SearchSalonPage/SalonContainer.jsx
+++ frontend/src/pages/SearchSalonPage/SalonContainer.jsx
@@ -1,10 +1,21 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { faImage } from "@fortawesome/free-solid-svg-icons";
+import { faThumbsUp, faThumbsDown } from "@fortawesome/free-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function SalonContainer({ salon }) {
   const navigate = useNavigate();
+  const [numLikes, setNumLikes] = useState(0);
+  const [numDislikes, setNumDislikes] = useState(0);
+
+  useEffect(() => {
+    setNumLikes(salon.reaction.filter((r) => r.response === "like").length);
+    setNumDislikes(
+      salon.reaction.filter((r) => r.response === "dislike").length
+    );
+  }, [salon.reaction]);
+
   return (
     <li
       className="border border-gray-300 rounded-md p-2 my-2"
@@ -24,6 +35,14 @@ export default function SalonContainer({ salon }) {
       )}
       <h2>{salon.salonname}</h2>
       <p>{salon.address}</p>
+      <div className="flex justify-between w-20">
+        <div>
+          <FontAwesomeIcon icon={faThumbsUp} /> {numLikes}
+        </div>
+        <div>
+          <FontAwesomeIcon icon={faThumbsDown} /> {numDislikes}
+        </div>
+      </div>
     </li>
   );
 }
